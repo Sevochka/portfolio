839df76958665a8afabff00e270400798ddb498c
Vsevolod <sevkaplay@gmail.com>
2021-04-17 15:56:43 +0300

    Set up react-refresh-webpack-plugin

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,9 +1,15 @@
 const webpack = require("webpack");
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
   mode: 'development',
+  devServer: {
+    hot: true,
+    open: true
+  },
   devtool: 'cheap-module-source-map',
   plugins: [
+    new ReactRefreshWebpackPlugin(),
     new webpack.DefinePlugin({
       'process.env.name': JSON.stringify('development'),
     }),
